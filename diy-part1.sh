4a85a4dd47b73d767dcd688b3c67bd1c5336c47f
WRR2001 <74068138+WRR2001@users.noreply.github.com>
2021-05-27 21:59:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 
-git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/Zxilly/JGSWeb package/JSGWeb
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 
